1440a16282eed5579cfa86c5672e9cb70061130d
Nic Schumann <nicolas_schumann@brown.edu>
2015-04-03 16:01:38 -0400

    client->server communication registeredl; text box clearing

--- definitions.js
+++ definitions.js
@@ -1,11 +1,17 @@
 
 module.exports = function( io ) {
 
+	function newMessageHandler( msg ) {
+		console.log( 'message-received: ' + msg );
+	}
+
 	return {
 		init: function( socket ) {
 			console.log('init handler');
 			socket.on('disconnect', function() { console.log('disconnect'); } );
+
+			socket.on('new-message', newMessageHandler );
 		}
 	};
-		
+
 };
--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,2 +1,18 @@
 var socket = io();
 
+
+
+$(document).ready( function() {
+	var messageBox = $( '#message-box' );
+
+	function inputhandler( event ) {
+		if ( event.keyCode != 13 ) return;
+
+		socket.emit('new-message', messageBox.val());
+
+		messageBox.val('');
+	}
+
+
+	messageBox.on('keyup', inputhandler );
+});
